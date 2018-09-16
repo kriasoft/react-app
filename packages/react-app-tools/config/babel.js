2d7b6ad7c8dfd6ee664b339e9b9246ce3244dcb1
Konstantin Tarkus <hello@tarkus.me>
2018-09-16 11:16:39 +0300

    Tweak

--- packages/react-app-tools/config/babel.js
+++ packages/react-app-tools/config/babel.js
@@ -18,11 +18,18 @@ const validateBoolOption = (name, value, defaultValue) => {
   return value;
 };
 
-module.exports = function(api, opts, env) {
+module.exports = function(api, opts) {
   if (!opts) {
     opts = {};
   }
 
+  // This is similar to how `env` works in Babel:
+  // https://babeljs.io/docs/usage/babelrc/#env-option
+  // We are not using `env` because it’s ignored in versions > babel-core@6.10.4:
+  // https://github.com/babel/babel/issues/4539
+  // https://github.com/facebook/create-react-app/issues/720
+  // It’s also nice that we can enforce `NODE_ENV` being specified.
+  var env = process.env.BABEL_ENV || process.env.NODE_ENV;
   var isEnvDevelopment = env === 'development';
   var isEnvProduction = env === 'production';
   var isEnvTest = env === 'test';
